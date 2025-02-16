9acb42c4316e417f448199775d0e2d7d7b793b03
lucca vinicios <vini.lucca42@gmail.com>
2025-02-15 21:08:26 -0300

    add render

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ def get_alert():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+poetry run gunicorn -b 0.0.0.0:10000 app:app
